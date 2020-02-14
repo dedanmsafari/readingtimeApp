437e0dede68078c863e7ea0e5eb7982d0a3788ec
Dedan Msafari <dedan.msafari@gmail.com>
2020-02-14 11:11:13 +0300

    edited app component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import Readingtime from '../src/components/readingtime';
 class App extends Component {
 
+  constructor(props){
+super(props);
+this.state = {text:'Today is a great day'}
+ this.handleChange = this.handleChange.bind(this)
+  }
+
+ handleChange(e) {
+  this.setState({text: e.target.value});
+}
   render() { 
     return (  
-    < Readingtime />
+      <div className="container-fluid">
+      <h1>Reading time</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-group">
+            <textarea 
+            
+            value={this.state.text}
+            onChange={this.handleChange}
+            id="readingtextarea" cols="30" rows="10"></textarea>
+          </div>
+        </div>
+        <div className="col-md-6">
+        < Readingtime  text={this.state.text} />
+        </div>
+      </div>
+    </div> 
     );
   }
 }
