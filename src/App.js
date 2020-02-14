ce1dd182baba09e4e72b0d48a1f835c35eb0ecd1
Dedan Msafari <dedan.msafari@gmail.com>
2020-02-14 12:33:06 +0300

    minor bootstrap changes

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ this.state = {text:'Today is a great day'}
 }
   render() { 
     return (  
-      <div className="container-fluid">
-      <h1>Reading time</h1>
+      <div className="container">
+      <h4>Basic Reading time App </h4>
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-8">
           <div className="form-group">
             <textarea 
             
             value={this.state.text}
             onChange={this.handleChange}
-            id="readingtextarea" cols="30" rows="10"></textarea>
+            id="readingtextarea" cols="105" rows="15"></textarea>
           </div>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-4">
         < Readingtime  text={this.state.text} />
         </div>
       </div>
